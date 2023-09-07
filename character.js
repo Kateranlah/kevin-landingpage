85e8ca26098b0bc68b0e308e0cef02deee2071fc
Kevin Sven Lentz <kevin.s.lentz@web.de>
2023-09-07 11:15:09 +0200

    added speaking bubble w/o function

--- character.js
+++ character.js
@@ -1,9 +1,22 @@
 const character = document.getElementById("character");
 
+function greet() {
+  speakAnimation();
+}
+
+function speakAnimation() {
+  setTimeout(() => {
+    startAnimation();
+    setTimeout(() => {
+      stopAnimation();
+    }, 5000);
+  }, 5000);
+}
+
 function startAnimation() {
-    document.getElementById("character").classList.add('animate'); 
+  document.getElementById("character").classList.add("animate");
 }
 
 function stopAnimation() {
-    document.getElementById("character").classList.remove('animate');
+  document.getElementById("character").classList.remove("animate");
 }
