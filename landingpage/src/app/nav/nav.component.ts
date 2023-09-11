8ebfcda4c2b78219afcc6955e8368a8ef213893a
Kevin Sven Lentz <kevin.s.lentz@web.de>
2023-09-11 14:04:41 +0200

    created simple nav. deleted old idea

--- landingpage/src/app/nav/nav.component.ts
+++ landingpage/src/app/nav/nav.component.ts
@@ -8,40 +8,16 @@ import { ProgressService }from '../progress.service'
 })
 export class NavComponent {
   @ViewChild('dot1') dot1!: ElementRef;
-  @ViewChild('dot2') dot2!: ElementRef;
-  @ViewChild('dot3') dot3!: ElementRef;
-  @ViewChild('ul') ul!: ElementRef;
-  @ViewChild('li1') li1!: ElementRef;
-  @ViewChild('li2') li2!: ElementRef;
-  @ViewChild('li3') li3!: ElementRef;
+ 
 
 
   progressNumber:number
   constructor(private ProgressService: ProgressService){this.ProgressService.getProgressObservable().subscribe((progress) => {
     this.progressNumber = progress; 
   if (progress === 2) {
-    this.deleteDots();
     } });
-  
-    
-  
   }
 
-  deleteDots(){
-    this.dot1.nativeElement.classList.add("dot-delete");
-    this.dot2.nativeElement.classList.add("dot-delete");
-    this.dot3.nativeElement.classList.add("dot-delete");
-    setTimeout(() => {
-      this.raiseNav();
-    }, 740);
-  }
-
-  raiseNav(){
-    this.li1.nativeElement.classList.add("collapse-nav");
-    this.li2.nativeElement.classList.add("collapse-nav");
-    this.li3.nativeElement.classList.add("collapse-nav");
-   
-  }
  
   increseProgress() {
     this.ProgressService.increseProgress();
