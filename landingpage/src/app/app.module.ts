9d9d319b836862420ab0f9c6c38c13bdb1393e1d
Kevin Sven Lentz <kevin.s.lentz@web.de>
2023-09-11 19:28:56 +0200

    bug appeared

--- landingpage/src/app/app.module.ts
+++ landingpage/src/app/app.module.ts
@@ -1,7 +1,5 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
